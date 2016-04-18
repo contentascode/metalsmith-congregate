46d72de9bb57a1c341cd2cddbb9538fffbae2013
Jun Matsushita <jun@iilab.org>
2016-04-18 12:40:02 +0200

    Added possibility to congregate folders

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var fs = require('fs');
  * A Metalsmith plugin to bring multiple separate files to one location.
  *
  * @param {Object} options
- * @param {String|String[]=} options.files - A list of file paths
+ * @param {String|String[]=} options.files - A list of file or folder paths
  * @param {String} options.output - The output directory
  *
  * @return {Function} - The Metalsmith plugin, iterate over each of the
@@ -19,7 +19,6 @@ module.exports = function (options) {
       typeof options.files === 'string' ? [options.files] :
         ['/buld/']
   );
-
   if (typeof options.output !== 'string') {
     throw new Error('`options.output` is mandatory and has to be a string')
   }
@@ -37,18 +36,70 @@ module.exports = function (options) {
     options.files.forEach(function (filepattern, fileindex) {
       var name = path.join(options.output, path.basename(filepattern));
       fs.stat(filepattern, function (err, stats) {
-        if (err) return done(err);
-        fs.readFile(filepattern, function (err, buffer) {
-          if (err) return done(err);
-          var file = {};
+        if (err) return done(new Error(err));
 
-          file.contents = buffer;
-          file.mode = Mode(stats).toOctal();
+        if (stats.isDirectory()) {
 
-          files[name] = file;
-          countFilesDone(done);
-        });
+          walk(filepattern, function (err, results) {
+            if (err) return done(new Error(err));
+
+            var filesDone = 0,
+            countFilesWalkDone = function(done) {
+              filesDone++;
+              if(filesDone == results.length){
+                done();
+              }
+            };
+
+            results.forEach(function(filepath) {
+              var buffer = fs.readFileSync(filepath)
+
+              var file = {};
+
+              file.contents = buffer;
+              file.mode = Mode(stats).toOctal();
+              files[options.output + filepath.replace(filepattern, '')] = file;
+              countFilesWalkDone(done);
+
+            })
+          });
+        } else {
+          fs.readFile(filepattern, function (err, buffer) {
+            if (err) return done(new Error(err));
+            var file = {};
+
+            file.contents = buffer;
+            file.mode = Mode(stats).toOctal();
+
+            files[name] = file;
+            countFilesDone(done);
+          });
+        }
       });
     });
   }
+};
+
+var walk = function(dir, done) {
+  var results = [];
+  fs.readdir(dir, function(err, list) {
+    if (err) return done(err);
+    var i = 0;
+    (function next() {
+      var file = list[i++];
+      if (!file) return done(null, results);
+      file = dir + '/' + file;
+      fs.stat(file, function(err, stat) {
+        if (stat && stat.isDirectory()) {
+          walk(file, function(err, res) {
+            results = results.concat(res);
+            next();
+          });
+        } else {
+          results.push(file);
+          next();
+        }
+      });
+    })();
+  });
 };
